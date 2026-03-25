d228fd85fc8ce265c435da397698c9a93154a53e
Mr kuttusan <yedhukrishnanu2001@gmail.com>
2026-03-25 20:12:49 +0530

    Update __init__.py
    Bbj

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,20 +1,11 @@
-from aiohttp import web
-from .route import routes
 from asyncio import sleep 
 from datetime import datetime
 from database.users_chats_db import db
-from info import LOG_CHANNEL, URL, PREMIUM_LOGS
-import aiohttp
-import asyncio
+from info import LOG_CHANNEL, PREMIUM_LOGS
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
 
 async def check_expired_premium(client):
     while 1:
@@ -33,16 +24,3 @@ async def check_expired_premium(client):
                 print(e)
             await sleep(0.5)
         await sleep(1)
-
-async def keep_alive():
-    """Keep bot alive by sending periodic pings."""
-    async with aiohttp.ClientSession() as session:
-        while True:
-            await asyncio.sleep(298)
-            try:
-                async with session.get(URL) as resp:
-                    if resp.status != 200:
-                        logging.warning(f"⚠️ Ping Error! Status: {resp.status}")
-            except Exception as e:
-                logging.error(f"❌ Ping Failed: {e}")           
-
